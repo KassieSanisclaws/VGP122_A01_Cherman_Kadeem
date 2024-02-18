f4d788bd524cdb90d25f7116e57ebf75c24ea076
Kadeem Cherman (Kassie) <86675041+KassieSanisclaws@users.noreply.github.com>
2024-02-18 17:59:24 -0500

    update changes added player function for player hand, dealer fucntion updated and minor fixes to code.

--- VGP122_A01_Cherman_Kadeem/VGP122_A01_Cherman_Kadeem.cpp
+++ VGP122_A01_Cherman_Kadeem/VGP122_A01_Cherman_Kadeem.cpp
@@ -1,26 +1,37 @@
 // VGP122_A01_Cherman_Kadeem.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+// [3] Call the function of expected value of player_Stands when player is standing.
+
 #include <iostream>
 using namespace std;
 
 void dealer(int old_total, int old_aceCount, int cards, double probability, int cardDeck[], double dealer_Probability[]);
 
 int numOfSuits = 6;
+int faceUp_Card;
 
 int main()
 {
-    int i = 0, cardDeck[11]{};
+    int i = 0, j = 0, cardDeck[11]{};
     //Represents the number of aces in deck.
     double dealer_Probability[7]{};
     for (i = 1; i <= 9; i++)
         cardDeck[i] = 4 * numOfSuits;
         cardDeck[10] = 16 * numOfSuits;
         cardDeck[0] = 52 * numOfSuits;
-        dealer(5, 0, 1, 1.0, cardDeck, dealer_Probability);
-        for (i = 0; i < 6; i++) cerr << i << "\t" << dealer_Probability[i] << "\n";
+        for (i = 0; i <= 10; i++){
+            cout << "Face Up Card = " << i << "\n";
+            cardDeck[i]--;
+            cardDeck[0]--;
+            dealer(i, ( i == 1 ? 1:0), 1, 1.0, cardDeck, dealer_Probability);
 
-
+        for (j = 0; j <= 6; j++) 
+            cerr << j << "\t" << dealer_Probability[j] << "\n";
+          //Adds back cards after down passing cards dealer fashion:
+           cardDeck[i]++;
+           cardDeck[0]++;
+        }
     /*std::cout << "Hello World!\n";*/
 }
 
@@ -35,6 +46,50 @@ int main()
 //   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
 //   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
 
+//Player function 
+static double player_Stands(int player_Total, int player_PaceTimer, int player_Cards, int cardDeck[])
+{
+   double player_Stands = 0.0, dealers_Probability[7]{};
+   //Test-Case when player hasd a Black Jack:
+     if ((player_Total == 1) && (player_PaceTimer == 1) && (player_Cards == 2)) {
+        //Test-Case player has winning Black Jack cards!:6
+         if ((faceUp_Card > 1) && (faceUp_Card < 10)) {
+             return 1.5;
+         }
+         else {
+        //Call Dealer function 
+             dealer(faceUp_Card, (faceUp_Card == 1 ? 1 : 0), 1, 1.0, cardDeck, dealers_Probability);
+             return 1.5 * (dealers_Probability[0] + dealers_Probability[1] + dealers_Probability[2] + dealers_Probability[3] + dealers_Probability[4] + dealers_Probability[6]);
+         }
+      }
+      //Test-Case Player ghets a Bust in Balck Jack:
+       else if (player_Total > 21) {
+         return -1.0;
+      }
+      //Test-Case player has a soft hand which then player gets 10points added to their total:
+       else {
+         if ((player_Total <= 11) && (player_PaceTimer > 0)) {
+             player_Total += 10;
+       //Call Dealer function on player hand card count:
+             dealer(faceUp_Card, (faceUp_Card == 1 ? 1 : 0), 1, 1.0, cardDeck, dealers_Probability);
+             player_Stands = dealers_Probability[6];
+         //Probability of dealer Black Jack:
+             player_Stands -= dealers_Probability[5]; 
+           for (int i = 0; i <= 4; i++) {
+               if (player_Total > i + 17) {
+                   player_Stands + dealers_Probability[i];
+               }
+                else if (player_Total < i + 17) {
+                   player_Stands -= dealers_Probability[i];
+                }
+             }
+         }
+      }
+     //Return expected value of player_Stands:
+     return player_Stands;
+}
+
+
 /*
   Dealer Probability 
   0 = 17
@@ -46,6 +101,7 @@ int main()
   6 = bust you loose.
 */
 
+//Dealer function for card distribution from dealer deck and probability of dealer wins ratio of chance to hit on the ace through int itterations:
 static void dealer(int old_total, int old_aceCount, int cards, double probability,int cardDeck[], double dealer_Probability[])
 {
     int i, next_Card, new_Total = 0, new_AceAdded = 0;
